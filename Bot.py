4765098c1561397dbc903fd935397c4feee0295e
Beamer Devs <83333147+anon2004@users.noreply.github.com>
2024-03-02 16:36:24 +0000

    Heroku deploy

--- Bot.py
+++ Bot.py
@@ -1,5 +1,53 @@
 import tweepy
 import time
+import requests
+
+
+# List of URLs to fetch data from
+urls = [
+    "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks/Absa Bank Kenya Plc",
+    "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks/Safaricom",
+    "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks/KCB Group Limited",
+    "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks/Equity Group Holdings Limited",
+    "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks/Co-operative Bank of Kenya Limited",
+    "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks/Kenya Power & Lighting Company",
+    # Add more URLs here as needed
+]
+
+# Common headers for all requests
+headers = {
+    "X-RapidAPI-Key": "be623b818cmsh7d25d2e33a15fccp15ee38jsne2bef3c5c029",
+    "X-RapidAPI-Host": "nairobi-stock-exchange-nse.p.rapidapi.com"
+}
+
+# Open tweets.txt file in append mode
+with open('tweets.txt', 'a') as f:
+    # Loop through each URL
+    for url in urls:
+        # Fetch data from the URL
+        response = requests.get(url, headers=headers)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            if data:
+                item = data[0]
+                stock_name = item.get('name')
+                volume = item.get('volume')
+                price = item.get('price')
+                change = item.get('change')
+
+                # Format the tweet-like string
+                tweet = f"Company: {stock_name}\nStock Name: {stock_name}\nVolume: {volume}\nPrice: {price}\nChange: {change}\n\n"
+
+                # Write the tweet to tweets.txt file
+                f.write(tweet)
+                f.write('\n')
+                print("Data updated for:", stock_name)
+            else:
+                print("No data found in response:", url)
+        else:
+            print("Failed to fetch data from:", url)
 
 # (Your Twitter API Credentials here)
 consumer_key = "3dBuNo5M9bgpevzDk9hMUylYN"
